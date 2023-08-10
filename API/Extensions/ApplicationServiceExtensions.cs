ed55201579e60b84a0fb23c9c69acf7c370d948d
MrDancerOriginal <40193041+MrDancerOriginal@users.noreply.github.com>
2023-08-10 20:27:40 +0300

    Pre-deployment

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -9,28 +9,23 @@ namespace API.Extensions;
 
 public static class ApplicationServiceExtensions
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
-        IConfiguration config)
-    {
-        services.AddDbContext<DataContext>(options =>
-        {
-            options.UseSqlite(config.GetConnectionString("DefaultConnection"));
-        });
+  public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+      IConfiguration config)
+  {
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    //builder.Services.AddEndpointsApiExplorer();
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        //builder.Services.AddEndpointsApiExplorer();
+    services.AddCors();
 
-        services.AddCors();
+    services.AddScoped<ITokenService, TokenService>();
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    services.Configure<CloudniarySettings>(config.GetSection("CloudniarySettings"));
+    services.AddScoped<IPhotoService, PhotoService>();
+    services.AddScoped<LogUserActivity>();
+    services.AddSignalR();
+    services.AddSingleton<PresenseTracker>();
+    services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        services.AddScoped<ITokenService, TokenService>();
-        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-        services.Configure<CloudniarySettings>(config.GetSection("CloudniarySettings"));
-        services.AddScoped<IPhotoService, PhotoService>();
-        services.AddScoped<LogUserActivity>();
-        services.AddSignalR();
-        services.AddSingleton<PresenseTracker>();
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-        return services;
-    }
+    return services;
+  }
 }
